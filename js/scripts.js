27099a8cf54d6341a9050b398ea5943c51467e5a
Svea Wade <svealinneawade@gmail.com>
2020-10-29 12:07:17 -0700

    final commit for today

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,12 @@
 //Test: "Should convert main numbers to Roman symbols"
 //Expect: romanNumeral(50).toEqual(L)
 
+//Test: "Input from user should only be between 0 and 3999"
+//Expect: romanNumeral(4000).toEqual(False)
+
+//Test: "Should separate 1s, 10s, 100s and 1000s"
+//Expect: inputString(152).toEqual([100,50,2])
+
 //Business Logic//
 /*function romanNumeral(number) {
   if (number === 1) {
@@ -26,12 +32,18 @@
 
 
 function romanNumeral(number) {
-  let array = [[1,"I"], [5,"V"], [10, "X"], [50, "L"], [100, "C"], [500, "D"], [1000, "M"]]; 
-  for (i=0; i<array.length; i++){
-    if (number === array[i][0]) {
-    return array[i][1];
+  if (number>=0 && number <4000){
+    const inputString = number.split("");//"987"
+    for from 1s to 1000s ["9","8","7"]
+    let array = [[1,"I"], [5,"V"], [10, "X"], [50, "L"], [100, "C"], [500, "D"], [1000, "M"]]; 
+    for (i=0; i<array.length; i++){
+      if (number === array[i][0]) {
+        return array[i][1];
+      }
     }
-  }
+  } else {
+    return false
+    } 
 }
 
 //UI Logic
