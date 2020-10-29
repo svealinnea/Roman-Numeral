96ec63f7fa56a1b58091cce3210a2fe279717c17
Svea Wade <svealinneawade@gmail.com>
2020-10-29 10:46:45 -0700

    passed 2nd test
    Co-authored-by:Nataliya Zhuravleva<natalindria@gmail.com>
    Co-authored-by:Joseph Giunta <jgnohack@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,24 @@
 //Test: "Should convert 1 to I"
 //Expect: romanNumeral(1).toEqual(I)
 
+//Test: "Should convert main numbers to Roman symbols"
+//Expect: romanNumeral(50).toEqual(L)
+
 //Business Logic//
 function romanNumeral(number) {
-  return "I";
+  if (number === 1) {
+    return "I";
+  } else if (number === 5) {
+    return "V";
+  } else if (number === 10) {
+    return "X";
+  } else if (number === 50) {
+    return "L";
+  } else if (number === 100) {
+    return "C";
+  } else if (number === 500) {
+    return "D";
+  } else if (number === 1000) {
+    return "M";
+  }
 }
